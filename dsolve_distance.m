c1de5a5ca4e0a9e8cd80972f24886d58c00b4c1c
pelse862 <pelse862@student.liu.se>
2015-02-23 12:44:16 +0100

    Kraftdeen till trycket

--- dsolve_distance.m
+++ dsolve_distance.m
@@ -1,11 +1,12 @@
 syms s(t);
 airCoeff=1.28;
 dens=1.2041; %densitet
-area=0.0025; %tvärsnittsarea i m^2
+area=0.0025; %tvï¿½rsnittsarea i m^2
 mass=0.005;
 g=9.82;
 x=1:10
 Ds = diff(s)
 Ds_2=diff(s,2)
 hej = dsolve(Ds_2==g-(0.5*airCoeff*dens*area*(Ds^2))/mass, s(0)==0, Ds(0)==0)
+
 
--- dsolve_speed.m
+++ dsolve_speed.m
@@ -1,16 +1,20 @@
 syms s(t);
 airCoeff=1.28;
 dens=1.2041; %densitet
-area=0.0025; %tvärsnittsarea i m^2
+area=0.0025; %tvï¿½rsnittsarea i m^2
 mass=0.005;
 g=9.82;
 x=1:10
 Ds = diff(s)
+
 hej = dsolve(Ds==g-(0.5*airCoeff*dens*area*s)/mass, s(0)==0)
 
-%25.48584 - 25.48584*exp(-0.3853*t)
+%h = 25.48584 - 25.48584*exp(-0.3853*t);
 
-%%
+
+
+
+ 
 clear all;
 
 
